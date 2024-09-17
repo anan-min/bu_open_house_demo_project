a40ed9b4ce625e8b299216eceff13ae991834611
nut <nut999anan@hotmail.com>
2024-09-17 09:55:43 +0700

    fix all the errors

--- components/navbar/HamburgerButton.jsx
+++ components/navbar/HamburgerButton.jsx
@@ -10,7 +10,7 @@ const HamburgerButton = ({ onClick }) => {
       aria-controls="navbar-default"
       aria-expanded="false"
     >
-      <span class="sr-only">Open main menu</span>
+      <span className="sr-only">Open main menu</span>
       <svg
         className="w-5 h-5"
         aria-hidden="true"
@@ -20,9 +20,9 @@ const HamburgerButton = ({ onClick }) => {
       >
         <path
           stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
           d="M1 1h15M1 7h15M1 13h15"
         />
       </svg>
